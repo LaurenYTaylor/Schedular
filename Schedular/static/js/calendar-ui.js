e16ae991524e2681c88f27b682181134744da196
Vivian <z5062431@cse.unsw.edu.au>
2018-10-03 10:38:08 +1000

    Fixed up Vivian's horrible push

--- Schedular/static/js/calendar-ui.js
+++ Schedular/static/js/calendar-ui.js
@@ -7,8 +7,6 @@ $('html').on('click', function (e) {
       popoverElement.popover('hide');
 });
 
-allEvents = []; 
-
 $(document).ready(function() {
     console.log(window.location.pathname);
 
@@ -30,61 +28,21 @@ $(document).ready(function() {
     $(function() {
     $(".CreateButton").click(function() {
       if (validateForm()){
-
-        taskName = $('#tName').val();
-        dury = $('#dury').val();
-        category = $("#category").val();
         $('#myModal').modal('hide')
-        $("#list").append("<div class='task-drag'>" + taskName + "</div>");
+        $("#list").append("<div class='task-drag'>" + $('#tName').val() + "</div>");
         $('#tName').val('');
         $('#hiddenText').hide();
 
-        newTask = {name:taskName, duration: dury, category:category};
-
-        socket.emit('task_added', newTask);
-        allEvents.push(newTask);
       }
-
       $("#task-list .task-drag" ).each(function() {
-
-        name = "Hey";
-        name2 = $(this).text(); 
-        time = "04:00:00";
-        colour = "grey"; 
-
-        var arrayLength = allEvents.length;
-        for (var i = 0; i < arrayLength; i++) {
-          if(name2 == allEvents[i].name){
-           time = "0"+allEvents[i].duration + ":00:00";
-           category = allEvents[i].category;
-
-           if (category == "University"){
-            colour = "Blue";
-           }
-           else if(category == "Chores"){
-            colour = "Yellow";
-           }
-           else if (category == "Work"){
-            colour = "Black";
-           }
-
-           else{
-            colour = "grey";
-           }
-          
-          }
-
-        }
-
 
         // store data so the calendar knows to render an event upon drop
         $(this).data('event', {
-          title: name2, // use the element's text as the event title
+          title: $.trim($(this).text()), // use the element's text as the event title
           stick: true, // maintain when user navigates (see docs on the renderEvent method)
-          color: colour,
+          color: 'green',
           description: 'This is a cool event',
-          complete: false,
-          duration: time
+          complete: false
         });
 
         // make the event draggable using jQuery UI
@@ -146,6 +104,7 @@ $(document).ready(function() {
     });
 
     $('#task-list .task-drag').each(function() {
+
       // store data so the calendar knows to render an event upon drop
       $(this).data('event', {
         title: $.trim($(this).text()), // use the element's text as the event title
@@ -312,10 +271,10 @@ function validateForm() {
 
 
 $('wrap').on('click','#toggle',function (e) {
-	console.log(element);
+  console.log(element);
   $(".circle-loader").toggleClass("load-complete");
   $(".checkmark").toggle();
-  $("#showCompleted").slideToggle('slow', 	taskCompleted());
+  $("#showCompleted").slideToggle('slow',   taskCompleted());
 });
 
 //Mark the task as complete
@@ -375,5 +334,4 @@ var isEventOverDiv = function(x, y) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
-
   
